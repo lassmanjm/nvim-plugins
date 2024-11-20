b300bc3dc255b7b2cbf589432ba4c8c9eaceea66
lassmanjm <lassmanjm@gmail.com>
2024-11-20 17:57:26 +0000

    Add kylechui/nvim-surround to general

--- lua/plugins/general.lua
+++ lua/plugins/general.lua
@@ -69,4 +69,14 @@ return {
 		dependencies = { "nvim-lua/plenary.nvim" },
 		opts = { signs = false },
 	},
+	{
+		"kylechui/nvim-surround",
+		version = "*", -- Use for stability; omit to use `main` branch for the latest features
+		event = "VeryLazy",
+		config = function()
+			require("nvim-surround").setup({
+				-- Configuration here, or leave empty to use defaults
+			})
+		end,
+	},
 }

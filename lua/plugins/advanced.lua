2db1e4b02713df7a457833481fd24c1b20968895
lassmanjm <lassmanjm@gmail.com>
2025-04-11 11:25:46 -0400

    Add hover key mapping to nvim-lsp-config

--- lua/plugins/advanced.lua
+++ lua/plugins/advanced.lua
@@ -264,6 +264,9 @@ return {
 					--  Most Language Servers support renaming across files, etc.
 					map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
 
+					-- Show method or parameter under cursor info
+					map("<leader>si", vim.lsp.buf.hover, "[S]how [I]nfo")
+
 					-- Execute a code action, usually your cursor needs to be on top of an error
 					-- or a suggestion from your LSP for this to activate.
 					map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction", { "n", "x" })
